0b128b0a4a52b50f237bd13e3e538226c3e6f663
Asya404 <unkiplum@gmail.com>
2022-04-11 15:19:33 +0200

    section-goals, updated scroll

--- js/app.js
+++ js/app.js
@@ -4,24 +4,49 @@ $(window).on('scroll', function () {
         c = $(window).height();
 
     var scrollPercent = (s / (d - c)) * 100;
-    console.log(scrollPercent); //Displaying scroll percentage in console
+    console.log(scrollPercent);
 
+    document.querySelectorAll('.tools-btn').forEach(el => {
+        el.style.background = '#fff';
+    })
 
-    if (scrollPercent < 60) {
+    if (scrollPercent < 50) {
         document.querySelector('.discussion-box-wrapper').style.display = "block";
         document.querySelector('.health-box-wrapper').style.display = "none";
 
         document.querySelector('.discussion__text').style.display = "block";
         document.querySelector('.health__text').style.display = "none";
-       
+
+        document.querySelectorAll('.tools-btn')[0].style.background= "linear-gradient(53.37deg, rgba(5, 133, 225, 0.94) 0%, #374EC6 136.26%), linear-gradient(53.37deg, rgba(5, 133, 225, 0.94) 0%, #374EC6 136.26%)";
+        
     }
-    else if (scrollPercent > 60) {
+    else if (scrollPercent < 60 && scrollPercent > 50) {
         document.querySelector('.discussion-box-wrapper').style.display = "none";
         document.querySelector('.health-box-wrapper').style.display = "block";
 
         document.querySelector('.discussion__text').style.display = "none";
         document.querySelector('.health__text').style.display = "block";
 
+        document.querySelectorAll('.tools-btn')[1].style.background= "linear-gradient(53.37deg, rgba(5, 133, 225, 0.94) 0%, #374EC6 136.26%), linear-gradient(53.37deg, rgba(5, 133, 225, 0.94) 0%, #374EC6 136.26%)";
     }
+    else if (scrollPercent > 60) {
+        // document.querySelector('.discussion-box-wrapper').style.display = "none";
+        // document.querySelector('.health-box-wrapper').style.display = "block";
+
+        document.querySelector('.health__text').style.display = "none";
+        document.querySelector('.follow__text').style.display = "block";
+
+        document.querySelectorAll('.tools-btn')[2].style.background= "linear-gradient(53.37deg, rgba(5, 133, 225, 0.94) 0%, #374EC6 136.26%), linear-gradient(53.37deg, rgba(5, 133, 225, 0.94) 0%, #374EC6 136.26%)";
+    }
+
+    $(window).scroll(function() {    
+        var scroll = $(window).scrollTop();
+    
+        if (scroll <= 500) {
+            $(".section-tools").addClass("sticky");
+        } else {
+            $(".section-tools").removeClass("sticky");
+        }
+    });
 
 })

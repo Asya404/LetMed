c32cc5c3bd47dcf028a1df2aa0376738fc9b8242
Asya404 <unkiplum@gmail.com>
2022-04-27 22:02:50 +0200

    select styled, fixed mobile menu bug

--- js/app.js
+++ js/app.js
@@ -1,91 +1,49 @@
 const controller = new ScrollMagic.Controller();
 
 /* SCENE 1 */
-const scene1 = new ScrollMagic.Scene({
-    triggerElement: '.tools',
-    offset: -120,
-    duration: 240
-})
+const scene1 = new ScrollMagic.Scene({ triggerElement: '.tools', offset: -120, duration: 240 })
 .setClassToggle('.discussion-box-wrapper', 'show')
-
-const scene1a = new ScrollMagic.Scene({
-    triggerElement: '.tools',
-    offset: -120,
-    duration: 240
-})
+const scene1a = new ScrollMagic.Scene({ triggerElement: '.tools', offset: -120, duration: 240 })
 .setClassToggle('.tools-btn--1', 'blue-btn');
 
 
-
-
 /* SCENE 2 */
-const scene2 = new ScrollMagic.Scene({
-    triggerElement: '.tools',
-    offset: 120,
-    duration: 120
-})
+const scene2 = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
 .setClassToggle('.health-box-wrapper', 'show')
-
-const scene2a = new ScrollMagic.Scene({
-    triggerElement: '.tools',
-    offset: 120,
-    duration: 120
-})
+const scene2a = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
 .setClassToggle('.tools-btn--2', 'blue-btn');
-
-const scene2b = new ScrollMagic.Scene({
-    triggerElement: '.tools',
-    offset: 120,
-    duration: 120
-})
+const scene2b = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
 .setClassToggle('.discussion__text', 'hide');
-
-const scene2c = new ScrollMagic.Scene({
-    triggerElement: '.tools',
-    offset: 120,
-    duration: 120
-})
+const scene2c = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
 .setClassToggle('.health__text', 'show-block');
-
-
+const scene2d = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
+.setClassToggle('.section-tools__subtitle--1', 'hide');
+const scene2e = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
+.setClassToggle('.section-tools__subtitle--2', 'show-block');
 
 
 /* SCENE 3 */
-const scene3 = new ScrollMagic.Scene({
-    triggerElement: '.tools',
-    offset: 240
-})
+const scene3 = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
 .setClassToggle('.follow-box-wrapper', 'show')
-
-const scene3a = new ScrollMagic.Scene({
-    triggerElement: '.tools',
-    offset: 240
-})
+const scene3a = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
 .setClassToggle('.tools-btn--3', 'blue-btn');
-
-const scene3b = new ScrollMagic.Scene({
-    triggerElement: '.tools',
-    offset: 240
-})
+const scene3b = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
 .setClassToggle('.discussion__text', 'hide');
-
-const scene3c = new ScrollMagic.Scene({
-    triggerElement: '.tools',
-    offset: 240
-})
+const scene3c = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
 .setClassToggle('.health__text', 'hide');
-
-const scene3d = new ScrollMagic.Scene({
-    triggerElement: '.tools',
-    offset: 240
-})
+const scene3d = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
 .setClassToggle('.follow__text', 'show-block');
-
+const scene3e = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
+.setClassToggle('.section-tools__subtitle--2', 'hide');
+const scene3f = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
+.setClassToggle('.section-tools__subtitle--3', 'show-block');
+const scene3g = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
+.setClassToggle('.section-tools__subtitle--1', 'hide');
 
 
 // .addTo(controller);
 controller.addScene([
-    scene1, scene1a, scene2, scene2a, scene2b, scene2c, scene3, scene3a, scene3b, scene3c, scene3d
+    scene1, scene1a, scene2, scene2a, scene2b, scene2c, scene2d, scene2e, scene3, scene3a, scene3b, scene3c, scene3d, scene3e, scene3f, scene3g
 ]);
 
 
@@ -95,7 +53,6 @@ const questions = document.querySelector('.questions');
 
 questions.addEventListener('click', function (e) {
     const active = document.querySelector('.question-active');
-
     if (active && active != e.target.closest('.question')) {
         console.log(active)
         active.classList.remove('question-active');
@@ -104,6 +61,7 @@ questions.addEventListener('click', function (e) {
         e.target.closest('.question').classList.toggle('question-active');
     }
 })
+
 
 /* MOBILE NAV */
 const btn = document.querySelector('.nav-btn');
@@ -122,4 +80,8 @@ btn.addEventListener('click', () => {
 //     $('body,html').animate({ scrollTop: top }, 1000);
 //  });
 
-
+document.querySelector('.header__select').addEventListener('click', () => {
+    document.querySelector('.select__list').classList.toggle('show-block');
+    document.querySelector('.header__select').classList.toggle('login-frame');
+    document.querySelector('.select__arrow').classList.toggle('select-active')
+})

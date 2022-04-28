8556b83e0760876ac05047ecd0cb0c38a20f5d58
Asya404 <unkiplum@gmail.com>
2022-04-28 16:13:31 +0200

    smooth scroll

--- js/app.js
+++ js/app.js
@@ -1,54 +1,54 @@
+// ANIMATIONS
 const controller = new ScrollMagic.Controller();
 
-/* SCENE 1 */
+// SCENE 1
 const scene1 = new ScrollMagic.Scene({ triggerElement: '.tools', offset: -120, duration: 240 })
-.setClassToggle('.discussion-box-wrapper', 'show')
+    .setClassToggle('.discussion-box-wrapper', 'show')
 const scene1a = new ScrollMagic.Scene({ triggerElement: '.tools', offset: -120, duration: 240 })
-.setClassToggle('.tools-btn--1', 'blue-btn');
+    .setClassToggle('.tools-btn--1', 'blue-btn');
 
 
-/* SCENE 2 */
+// SCENE 2
 const scene2 = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
-.setClassToggle('.health-box-wrapper', 'show')
+    .setClassToggle('.health-box-wrapper', 'show')
 const scene2a = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
-.setClassToggle('.tools-btn--2', 'blue-btn');
+    .setClassToggle('.tools-btn--2', 'blue-btn');
 const scene2b = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
-.setClassToggle('.discussion__text', 'hide');
+    .setClassToggle('.discussion__text', 'hide');
 const scene2c = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
-.setClassToggle('.health__text', 'show-block');
+    .setClassToggle('.health__text', 'show-block');
 const scene2d = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
-.setClassToggle('.section-tools__subtitle--1', 'hide');
+    .setClassToggle('.section-tools__subtitle--1', 'hide');
 const scene2e = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
-.setClassToggle('.section-tools__subtitle--2', 'show-block');
+    .setClassToggle('.section-tools__subtitle--2', 'show-block');
 
 
-/* SCENE 3 */
+// SCENE 3
 const scene3 = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-.setClassToggle('.follow-box-wrapper', 'show')
+    .setClassToggle('.follow-box-wrapper', 'show')
 const scene3a = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-.setClassToggle('.tools-btn--3', 'blue-btn');
+    .setClassToggle('.tools-btn--3', 'blue-btn');
 const scene3b = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-.setClassToggle('.discussion__text', 'hide');
+    .setClassToggle('.discussion__text', 'hide');
 const scene3c = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-.setClassToggle('.health__text', 'hide');
+    .setClassToggle('.health__text', 'hide');
 const scene3d = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-.setClassToggle('.follow__text', 'show-block');
+    .setClassToggle('.follow__text', 'show-block');
 const scene3e = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-.setClassToggle('.section-tools__subtitle--2', 'hide');
+    .setClassToggle('.section-tools__subtitle--2', 'hide');
 const scene3f = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-.setClassToggle('.section-tools__subtitle--3', 'show-block');
+    .setClassToggle('.section-tools__subtitle--3', 'show-block');
 const scene3g = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-.setClassToggle('.section-tools__subtitle--1', 'hide');
+    .setClassToggle('.section-tools__subtitle--1', 'hide');
 
 
-// .addTo(controller);
 controller.addScene([
     scene1, scene1a, scene2, scene2a, scene2b, scene2c, scene2d, scene2e, scene3, scene3a, scene3b, scene3c, scene3d, scene3e, scene3f, scene3g
 ]);
 
 
 
-/* SECTION QUESTIONS */
+// SECTION QUESTIONS
 const questions = document.querySelector('.questions');
 
 questions.addEventListener('click', function (e) {
@@ -63,7 +63,8 @@ questions.addEventListener('click', function (e) {
 })
 
 
-/* MOBILE NAV */
+
+// MOBILE NAV
 const btn = document.querySelector('.nav-btn');
 const menu = document.querySelector('.nav-mobile');
 
@@ -73,15 +74,25 @@ btn.addEventListener('click', () => {
     document.querySelector('.overlay').classList.toggle('active-overlay');
 })
 
-// $("#menu, #logo, #services").on("click", "a", function (event) {
-//     event.preventDefault();
-//     var id = $(this).attr('href'),
-//        top = $(id).offset().top;
-//     $('body,html').animate({ scrollTop: top }, 1000);
-//  });
 
+
+// SELECT
 document.querySelector('.header__select').addEventListener('click', () => {
     document.querySelector('.select__list').classList.toggle('show-block');
     document.querySelector('.header__select').classList.toggle('login-frame');
     document.querySelector('.select__arrow').classList.toggle('select-active')
 })
+
+
+
+// SMOOTH SCROLL
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(smoothLink.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};

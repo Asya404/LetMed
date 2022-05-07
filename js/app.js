6d9c62f9089540d48c485332474de7a93e6089a8
Asya404 <unkiplum@gmail.com>
2022-05-08 01:50:04 +0200

    optimization of animations on scroll

--- js/app.js
+++ js/app.js
@@ -1,50 +1,74 @@
 // ANIMATIONS
 const controller = new ScrollMagic.Controller();
 
-// SCENE 1
-const scene1 = new ScrollMagic.Scene({ triggerElement: '.tools', offset: -120, duration: 240 })
-    .setClassToggle('.discussion-box-wrapper', 'show')
-const scene1a = new ScrollMagic.Scene({ triggerElement: '.tools', offset: -120, duration: 240 })
-    .setClassToggle('.tools-btn--1', 'blue-btn');
+
+const boxes = document.querySelectorAll('.box-wrapper')
+for(let i=0; i<boxes.length; i++) {
+
+    new ScrollMagic.Scene({ triggerElement: '.tools', offset: -120, duration: 240 })
+    .setClassToggle(boxes[0], "show")
+	.addTo(controller);  
+
+    new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
+    .setClassToggle(boxes[1], "show")
+	.addTo(controller);  
+    
+    new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
+    .setClassToggle(boxes[2], "show")
+	.addTo(controller); 
+}
 
 
-// SCENE 2
-const scene2 = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
-    .setClassToggle('.health-box-wrapper', 'show')
-const scene2a = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
-    .setClassToggle('.tools-btn--2', 'blue-btn');
-const scene2b = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
-    .setClassToggle('.discussion__text', 'hide');
-const scene2c = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
-    .setClassToggle('.health__text', 'show-block');
-const scene2d = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
-    .setClassToggle('.section-tools__subtitle--1', 'hide');
-const scene2e = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
-    .setClassToggle('.section-tools__subtitle--2', 'show-sub');
+const btns = document.querySelectorAll('.tools-btn')
+for(let i=0; i<btns.length; i++) {
+
+    new ScrollMagic.Scene({ triggerElement: '.tools', offset: -120, duration: 240 })
+    .setClassToggle(btns[0], "blue-btn")
+	.addTo(controller);  
+
+    new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
+    .setClassToggle(btns[1], "blue-btn")
+	.addTo(controller);  
+    
+    new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
+    .setClassToggle(btns[2], "blue-btn")
+	.addTo(controller); 
+}
 
 
-// SCENE 3
-const scene3 = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-    .setClassToggle('.follow-box-wrapper', 'show')
-const scene3a = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-    .setClassToggle('.tools-btn--3', 'blue-btn');
-const scene3b = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-    .setClassToggle('.discussion__text', 'hide');
-const scene3c = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-    .setClassToggle('.health__text', 'hide');
-const scene3d = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-    .setClassToggle('.follow__text', 'show-block');
-const scene3e = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-    .setClassToggle('.section-tools__subtitle--2', 'hide');
-const scene3f = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-    .setClassToggle('.section-tools__subtitle--3', 'show-sub');
-const scene3g = new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
-    .setClassToggle('.section-tools__subtitle--1', 'hide');
+const texts = document.querySelectorAll('.tools__text')
+for(let i=0; i<btns.length; i++) {
+
+    new ScrollMagic.Scene({ triggerElement: '.tools', offset: -120, duration: 240 })
+    .setClassToggle(texts[0], "show-block")
+	.addTo(controller);  
+
+    new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
+    .setClassToggle(texts[1], "show-block")
+	.addTo(controller);  
+    
+    new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
+    .setClassToggle(texts[2], "show-block")
+	.addTo(controller); 
+}
 
 
-controller.addScene([
-    scene1, scene1a, scene2, scene2a, scene2b, scene2c, scene2d, scene2e, scene3, scene3a, scene3b, scene3c, scene3d, scene3e, scene3f, scene3g
-]);
+const subtitles = document.querySelectorAll('.section-tools__subtitle')
+for(let i=0; i<btns.length; i++) {
+
+    new ScrollMagic.Scene({ triggerElement: '.tools', offset: -120, duration: 240 })
+    .setClassToggle(subtitles[0], "show-block")
+	.addTo(controller);  
+
+    new ScrollMagic.Scene({ triggerElement: '.tools', offset: 120, duration: 120 })
+    .setClassToggle(subtitles[1], "show-block")
+	.addTo(controller);  
+    
+    new ScrollMagic.Scene({ triggerElement: '.tools', offset: 240 })
+    .setClassToggle(subtitles[2], "show-block")
+	.addTo(controller); 
+}
+
 
 
 

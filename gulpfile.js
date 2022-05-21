4e670fa16f45dcd4e05e656c351657ffad89aa54
Asya404 <unkiplum@gmail.com>
2022-05-21 20:56:44 +0200

    merged two LP

--- gulpfile.js
+++ gulpfile.js
@@ -5,9 +5,16 @@ const rename = require('gulp-rename');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 const concat = require('gulp-concat');
+const webp = require('gulp-webp');
 
 const cssFiles = [
-    './sass/main.scss',
+    './sass/sassLetmed/main.scss',
+    './owlcarousel/owl.carousel.min.css',
+    './owlcarousel/owl.theme.default.min.css'
+]
+
+const cssFilesCoop = [
+    './sass/sassCoop/main.scss',
     './owlcarousel/owl.carousel.min.css',
     './owlcarousel/owl.theme.default.min.css'
 ]
@@ -25,8 +32,31 @@ async function styles() {
         .pipe(gulp.dest('./css'));
 }
 
-gulp.task('styles', styles);
 
-gulp.task('sass:watch', async function () {
-    gulp.watch('./sass/**/*.scss', gulp.series('styles'))
-})
+async function stylesCoop() {
+    return gulp.src(cssFilesCoop)
+        .pipe(concat('all.css'))
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer())
+        .pipe(cleanCSS({
+            compatibility: 'ie8',
+            level: 2
+        }))
+        .pipe(rename('styleCoop.css'))
+        .pipe(gulp.dest('./css'));
+}
+
+async function img2Webp() {
+    return gulp.src('./img/*.jpg')
+        .pipe(webp())
+        .pipe(gulp.dest('./img'));
+}
+
+
+gulp.task('styles', styles);
+gulp.task('stylesCoop', stylesCoop);
+
+gulp.task('watch', async function () { gulp.watch('./sass/**/*.scss', gulp.series('styles')) })
+gulp.task('watchCoop', async function () { gulp.watch('./scssCoop/**/*.scss', gulp.series('stylesCoop')) })
+
+gulp.task('images', img2Webp)
---
+++ js/appCoop.js
@@ -0,0 +1,101 @@
+import langObj from './translateCoop.js';
+
+
+const select = document.querySelectorAll('.select');
+const selectEn = document.querySelectorAll('.select__lang--en');
+const selectPl = document.querySelectorAll('.select__lang--pl');
+
+// SELECT ARROW AND FRAME
+select.forEach(el => {
+    el.addEventListener('click', () => {
+        el.classList.toggle('login-frame');
+        el.lastElementChild.classList.toggle('select-active');
+        el.firstElementChild.lastElementChild.classList.toggle('arrow-active')
+    })
+})
+
+
+// Translates to en only if it's open
+selectEn.forEach(el => {
+    el.addEventListener('click', function () {
+        if (this.parentElement.classList.contains('login-frame')) {
+            getTranslate('en')
+        }
+    });
+})
+
+selectPl.forEach(el => {
+    el.addEventListener('click', function () {
+        getTranslate('pl')
+    });
+})
+
+
+function getTranslate(lang) {
+    document.querySelectorAll('[data-lang]').forEach(el => {
+        if (el.dataset.lang) {
+            el.textContent = langObj[lang][el.dataset.lang]
+        }
+        if (el.placeholder) {
+            el.placeholder = langObj[lang][el.dataset.lang];
+            el.textContent = ''
+        }
+
+        if (lang == 'pl') {
+            document.querySelector('.hero__heading').style.maxWidth = '890px';
+        } else if (lang == 'en') {
+            document.querySelector('.hero__heading').style.maxWidth = '766px';
+        }
+    })
+}
+
+
+
+
+// ANIMATIONS
+var controller = new ScrollMagic.Controller();
+
+// loop through all elements
+$('.fade-in').each(function() {
+  
+  // build a tween
+  var tween = TweenMax.from($(this), 0.7, {autoAlpha: 0, y: '+=100', x: '0', ease:Linear.easeNone});
+
+  // build a scene
+  var scene = new ScrollMagic.Scene({
+    triggerElement: this,
+	triggerHook: 0.7,
+	offset: 0,
+    reverse: false
+  })
+  .setTween(tween)
+  .addTo(controller);
+  
+});
+
+
+
+// MOBILE NAV
+const btn = document.querySelector('.nav__btn');
+const menu = document.querySelector('.nav-mobile');
+
+btn.addEventListener('click', () => {
+    btn.classList.toggle('open');
+    menu.classList.toggle('active');
+    document.querySelector('.overlay').classList.toggle('active-overlay');
+})
+
+
+
+
+// SMOOTH SCROLL
+const smoothLinks = document.querySelectorAll('a[href^="#"]');
+for (let smoothLink of smoothLinks) {
+    smoothLink.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(smoothLink.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+};
